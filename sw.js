45c56b1d99ce762a08fca3b5b384327de3eda557
ooosmanooosman2029-wq <ooosmanooosman2029@gmail.com>
2026-06-20 05:45:07 +0300

    Add PWA support

--- sw.js
+++ sw.js
@@ -1,9 +1,11 @@
-const CACHE_NAME = "mylife-v1";
+const CACHE_NAME = "mylife-v2";
 
 const urlsToCache = [
   "./",
   "./index.html",
-  "./manifest.json"
+  "./manifest.json",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
 ];
 
 self.addEventListener("install", event => {
@@ -13,9 +15,25 @@ self.addEventListener("install", event => {
   );
 });
 
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
+
 self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => response || fetch(event.request))
+      .then(response => {
+        return response || fetch(event.request);
+      })
   );
 });
