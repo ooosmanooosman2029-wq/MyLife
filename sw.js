9e49111c70ba3b2ad4792833c52d31d929a991e6
ooosmanooosman2029-wq <ooosmanooosman2029@gmail.com>
2026-06-23 06:54:49 +0300

    adding new features 2

--- sw.js
+++ sw.js
@@ -1,13 +1,39 @@
-const CACHE_NAME = "mylife-v3";
-const urlsToCache = ["./", "./index.html", "./manifest.json"];
+const CACHE_NAME = "mylife-v2";
+
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
+];
+
 self.addEventListener("install", event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
-  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
 });
+
 self.addEventListener("activate", event => {
-  event.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE_NAME && caches.delete(k)))));
-  self.clients.claim();
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
 });
+
 self.addEventListener("fetch", event => {
-  event.respondWith(caches.match(event.request).then(r => r || fetch(event.request).catch(() => caches.match("./"))));
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
 });
